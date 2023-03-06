9a858ed8f24606b847496cde3f0b8c61757188fa
Murilo Alves Melo <murilo.melo@secad.local>
2023-03-06 17:29:38 -0300

    Lista de operadores

---
+++ src/characters.js
@@ -0,0 +1,20 @@
+const characters = [
+    {
+        nick: 'Brava',
+        image: 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/7piw6czbaAsv6NnYXtJZay/02683e035ab9481bbe8cbf669113c84d/r6s-operators-list-brava.png'
+    },
+    {
+        nick: 'Grim',
+        image: 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/7fwxcTrdNLQbXkfBJglLaN/38d901042d7debc709c266a46f856d7e/r6s-operators-list-grim.png'
+    },
+    {
+        nick: 'Sens',
+        image: 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/1ieRqIy6e47QH48sTp1W4a/59f8188d94ae610bf76da26b4fef0b92/r6s-operators-list-sens.png'
+    },
+    {
+        nick: 'Osa',
+        image: 'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/3Dg95rvyhPtw588r60vIPM/75e609068a0b205cc4dbc7bf3e517f51/r6s-operators-list-osa.png'
+    },
+]
+
+export default characters
